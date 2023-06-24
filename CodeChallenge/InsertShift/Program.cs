b4226022ffcfa7aef13d7646d10c2d83ff633912
Bashar Nidal Shukri Shehadeh <1934674@std.hu.edu.jo>
2023-06-24 19:50:09 +0300

    Linked list upd

--- CodeChallenge/InsertShift/Program.cs
+++ CodeChallenge/InsertShift/Program.cs
@@ -6,10 +6,20 @@ namespace InsertShift
     {
         static void Main(string[] args)
         {
-          
 
+            LinkedList link = new LinkedList();
+            link.Insert(5);
+            link.Insert(6);
+            link.Insert(7);
+            link.Insert(8);
+            link.Insert(9);
+           
+            Console.WriteLine(link.Include(8));
+            Console.WriteLine(link.ToString());
+           
         }
 
-      
+
+        
     }
 }
