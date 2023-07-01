04c0f5e9b5589661f04f83c301e6c3e8955ff3fc
Bashar Shehadeh <83985765+bashar-27@users.noreply.github.com>
2023-07-02 02:59:49 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
+﻿using System;
 
-(Link)[CodeChallenge/InsertShift]
+namespace InsertShift
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            LinkedList link = new LinkedList();
+            link.Insert(5);
+            link.Insert(6);
+            link.Insert(7);
+            link.Insert(8);
+            link.InsertAfter(8, 20);
+            link.Insert(9);
+            link.InsertBefore(9, 55);
+            Console.WriteLine(link.Include(8));
+            Console.WriteLine(link.ToString());
+           
+        }
+
+
+
+    }
+}
