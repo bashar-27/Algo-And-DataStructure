ccb59f2dafbef5baab81621a133c970231f8da59
Bashar Nidal Shukri Shehadeh <1934674@std.hu.edu.jo>
2023-07-19 17:33:12 +0300

    CC12 shelter

---
+++ CodeChallenge/InsertShift/AnimalShelter.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsertShift
+{
+    public class AnimalShelter
+    {
+        private Queue<Animal> Dogs;
+        private Queue<Animal> Cats;
+        public AnimalShelter()
+        {
+            Dogs = new Queue<Animal>();
+            Cats = new Queue<Animal>();
+        }
+        public void Enqueue (Animal animal)
+        {
+            if(animal.species.ToLower() == "cat") {
+                Cats.Enqueue(animal);
+            }
+            if(animal.species.ToLower() == "dog")
+            {
+                Dogs.Enqueue(animal);
+            }
+        }
+        public Animal Dequeue (string pref)
+        {
+            if(pref == "cat")
+            {
+                return Cats.Dequeue();
+            }
+            else if(pref == "dog")
+            {
+                return Dogs.Dequeue();
+            }
+            else throw new ArgumentException("Invalid preference. Only Dogs: 'dog', or Cats: 'cat' allowed.");
+            
+
+        }
+        public void countDog(List <Animal>dogAnimal)
+        {
+            int i = 1;
+            Console.WriteLine("The Number Of DOGS Is :" + Dogs.Count);
+           foreach(Animal animal in Dogs)
+            {
+                Console.WriteLine(i+"_" + animal.Name+" ");
+                i++;
+            }
+           
+        }
+        public void countCat(List<Animal> catAnimal)
+        {
+            int i = 1;
+            Console.WriteLine("The Number Of CATS Is :" + Cats.Count);
+            foreach (Animal animal in Cats)
+            {
+                Console.WriteLine(i + "-" + animal.Name + " ");
+                i++;
+            }
+
+        }
+    }
+    
+}
--- CodeChallenge/InsertShift/Program.cs
+++ CodeChallenge/InsertShift/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace InsertShift
 {
@@ -6,19 +7,43 @@ namespace InsertShift
     {
         static void Main(string[] args)
         {
+            AnimalShelter animalShelter = new AnimalShelter();
+            Animal Dog = new Animal() { species = "dog", Name = "Hasky" };
+            Animal Dog2 = new Animal() { species = "DOG", Name = "pitBull" };
+            Animal Cat1 = new Animal() { species = "Cat", Name = "shirazi" };
+            Animal Cat2 = new Animal() { species = "cat", Name = "siami" };
+            Animal Cat3 = new Animal() { species = "cat", Name = "Britch" };
+            List<Animal> DogsLst = new List<Animal>();
+            List<Animal> CatsLst = new List<Animal>();
+            Console.WriteLine("Add Dogs");
+            animalShelter.Enqueue(Dog);
+            DogsLst.Add(Dog);
+            animalShelter.Enqueue(Dog2);
+            DogsLst.Add(Dog2);
+            animalShelter.countDog(DogsLst);
 
 
-            LinkedList link = new LinkedList();
-            link.Insert(5);
-            link.Insert(6);
-            link.Insert(7);
-            link.Insert(8);
-            link.InsertAfter(8, 20);
-            link.Insert(9);
-            link.InsertBefore(9, 55);
-            Console.WriteLine(link.Include(8));
-            Console.WriteLine(link.ToString());
-           
+            Console.WriteLine("\nAdd Cats ");
+            animalShelter.Enqueue(Cat1);
+            CatsLst.Add(Cat1);
+            animalShelter.Enqueue(Cat2);
+            CatsLst.Add(Cat2);
+            animalShelter.Enqueue(Cat3);
+            CatsLst.Add(Cat3);
+            animalShelter.countCat(CatsLst);
+
+            Console.WriteLine("\n\nRemove Dogs");
+            animalShelter.Dequeue(Dog.species);
+            DogsLst.Remove(Dog);
+            animalShelter.countDog(DogsLst);
+
+
+            Console.WriteLine("\nRemove Cats");
+            animalShelter.Dequeue(Cat3.species);
+            DogsLst.Remove(Cat3);
+            animalShelter.countCat(CatsLst);
+
+
         }
 
 
