06455b1fe8bc36b814d05448ab4156313f2cd503
Bashar Nidal Shukri Shehadeh <1934674@std.hu.edu.jo>
2023-07-23 16:39:04 +0300

    Valid brackers

--- CodeChallenge/InsertShift/Program.cs
+++ CodeChallenge/InsertShift/Program.cs
@@ -4,21 +4,21 @@ namespace InsertShift
 {
     public class Program
     {
+        
+
         static void Main(string[] args)
         {
 
 
-            LinkedList link = new LinkedList();
-            link.Insert(5);
-            link.Insert(6);
-            link.Insert(7);
-            link.Insert(8);
-            link.InsertAfter(8, 20);
-            link.Insert(9);
-            link.InsertBefore(9, 55);
-            Console.WriteLine(link.Include(8));
-            Console.WriteLine(link.ToString());
-           
+            string bracket = "{}";
+            Console.WriteLine(validateBrackets.IsvalidateBrackets(bracket));
+            string bracket2 = "{[}";
+            Console.WriteLine(validateBrackets.IsvalidateBrackets(bracket2));
+            string bracket3 = "(";
+            Console.WriteLine(validateBrackets.IsvalidateBrackets(bracket3));
+            string bracket4 = "{()[{}]}";
+            Console.WriteLine(validateBrackets.IsvalidateBrackets(bracket4));
+
         }
 
 
