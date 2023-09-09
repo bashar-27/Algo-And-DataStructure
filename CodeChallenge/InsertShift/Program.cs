4f63f7a26df88888fe99eb6dfd42e447b8c8f2c3
Bashar Nidal Shukri Shehadeh <1934674@std.hu.edu.jo>
2023-09-09 20:58:28 +0300

    committ

--- CodeChallenge/InsertShift/Program.cs
+++ CodeChallenge/InsertShift/Program.cs
@@ -13,173 +13,116 @@ namespace InsertShift
 
         static void Main(string[] args)
         {
-            int[] arr = { 8, 4, 23, 42, 16, 15 };
-            int n = arr.Length;
-            Console.WriteLine("Array before call function"); 
-            Print(arr, n);
-            int [] mergedArr = MergeSort(arr,0,n-1);
-            Console.WriteLine("Array after call function"); 
-            Print(mergedArr, n);
-          
+            Hashtable hashtable = new Hashtable();
 
+            hashtable.Set("name", "Bashar");
+            hashtable.Set("age", 23);
+            hashtable.Set("city", "Amman");
+
+            Console.WriteLine("Name: " + hashtable.Get("name"));
+            Console.WriteLine("Age: " + hashtable.Get("age"));
+            Console.WriteLine("Has city: " + hashtable.Has("city"));
+            Console.WriteLine("Has country: " + hashtable.Has("country"));
+
+            List<string> keys = hashtable.Keys();
+            Console.WriteLine("Keys: " + string.Join(", ", keys));
         }
-        public static int[] Merge(int[] arr, int left, int mid, int right)
+    }
+
+    public class KeyValue
+    {
+        public string Key { get; set; }
+        public object Value { get; set; }
+    }
+
+    public class Hashtable
+    {
+        public const int Size = 100;
+        public List<List<KeyValue>> table;
+
+        public Hashtable()
         {
-            int i, j = 0;
-            int k = left;
-            int lArr = mid - left + 1;              //to find size left array
-            int rArr = right - mid;                 //to find size right array
-            int[] leftSubArr = new int[lArr];       //new sub left array with lArr size
-            int[] rightSubArr = new int[rArr];      //new sub right array with rArr size
-
-            for (i = 0; i < lArr; i++)
-                leftSubArr[i] = arr[left + i];      //for fill new sub left array
-            for (j = 0; j < rArr; j++)
-                rightSubArr[j] = arr[mid + 1 + j];  //for fill new sub right array
-
-            i = 0;
-            j = 0;
-            while (i < lArr && j < rArr)
+            table = new List<List<KeyValue>>(Size);
+            for (int i = 0; i < Size; i++)
             {
-                if (leftSubArr[i] <= rightSubArr[j])
-                {
-                    arr[k] = leftSubArr[i];
-                    i++;
-                }
-                else
-                {
-                    arr[k] = rightSubArr[j];
-                    j++;
-                }
-                k++;
+                table.Add(new List<KeyValue>());
             }
-            //if one side of array have no elements and other side still have an elements
-            while (i < lArr)
-            {
-                arr[k] = leftSubArr[i];
-                i++;
-                k++;
-            }
-            while (j < rArr)
-            {
-                arr[k] = rightSubArr[j];
-                j++;
-                k++;
-            }
-
-            return arr;
         }
 
-       
-        public static int[] MergeSort(int[] arr, int left, int right)
+        public int CalculateHash(string key)
         {
-            if (left < right)
+            int hash = 0;
+            foreach (char c in key)
             {
-                int mid = left + (right - left) / 2;
-                MergeSort(arr, left, mid);
-                MergeSort(arr, mid + 1, right);
-                arr = Merge(arr, left, mid, right);
+                hash += c;
             }
-
-            return arr;
+            return hash % Size;
         }
-    
-    public static void Print(int[]arr ,int n)
+
+        public void Set(string key, object value)
         {
-            for (int i = 0; i < n; i++)
+            int index = CalculateHash(key);
+            List<KeyValue> bucket = table[index];
+
+            foreach (var kvp in bucket)
             {
-                Console.Write(arr[i]+" ");
-            }
-            Console.WriteLine();
-        }
-    
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        public static int[] InsertionSort(int[] arr)
-        {
-            int key, x;
-            for (int i = 0; i < arr.Length; i++)
-            {
-                key = arr[i];
-                x = i - 1;
-                while (x >= 0 && arr[x] > key)
+                if (kvp.Key == key)
                 {
-                    arr[x + 1] = arr[x];
-                    x--;
-                }
-                arr[x+1] = key;
-
-            }
-            return arr;
-        }
-        public static List<string> FizzBuzzTree(BinarySearchTree tree)
-        {
-            List<string> resultList = new List<string>();
-            PreOrderr(tree.root, resultList);
-            return resultList;
-        }
-
-
-        /// <summary>
-        /// Method to check for every Nodes in a binary tree
-        /// </summary>
-        /// <param name="root">Root node checking for leftchild and rightchild</param>
-        public static void PreOrderr(Node root, List<string> result)
-        {
-
-
-            if (root != null)
-            {
-                if (root.Data % 15 == 0)
-                {
-                    result.Add("FizzBuzz");
-                }
-                else if (root.Data % 3 == 0)
-                {
-                    result.Add("Fizz");
-
-                }
-                else if (root.Data % 5 == 0)
-                {
-                    result.Add("Buzz");
-
-                }
-                else
-                {
-                    result.Add(root.Data.ToString());
-                }
-                if(root.left != null)
-                {
-                    PreOrderr(root.left, result);
-                }
-               if (root.right != null)
-                {
-
-                PreOrderr(root.right, result);
+                    kvp.Value = value;
+                    return;
                 }
             }
 
-
-
-
-
-
-
-
+            bucket.Add(new KeyValue { Key = key, Value = value });
         }
 
-   
+        public object Get(string key)
+        {
+            int index = CalculateHash(key);
+            List<KeyValue> bucket = table[index];
+
+            foreach (var kvp in bucket)
+            {
+                if (kvp.Key == key)
+                {
+                    return kvp.Value;
+                }
+            }
+
+            return null;
+        }
+
+        public bool Has(string key)
+        {
+            int index = CalculateHash(key);
+            List<KeyValue> bucket = table[index];
+
+            foreach (var kvp in bucket)
+            {
+                if (kvp.Key == key)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public List<string> Keys()
+        {
+            List<string> keys = new List<string>();
+
+            foreach (var bucket in table)
+            {
+                foreach (var kvp in bucket)
+                {
+                    keys.Add(kvp.Key);
+                }
+            }
+
+            return keys;
+        }
+
     }
 }
-
-
-
-    
+//Hash Function
