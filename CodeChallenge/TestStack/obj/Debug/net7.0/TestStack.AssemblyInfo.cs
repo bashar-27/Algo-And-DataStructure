dec6793087e8e80cd25831b7c0c60a00f141015d
Bashar Nidal Shukri Shehadeh <1934674@std.hu.edu.jo>
2023-09-25 17:50:05 +0300

    cc

--- CodeChallenge/TestStack/obj/Debug/net7.0/TestStack.AssemblyInfo.cs
+++ CodeChallenge/TestStack/obj/Debug/net7.0/TestStack.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestStack")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2058f05b7b983387fb16ee50267c21fbde91404")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestStack")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestStack")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- CodeChallenge/TreeTest/obj/Debug/net7.0/TreeTest.AssemblyInfo.cs
+++ CodeChallenge/TreeTest/obj/Debug/net7.0/TreeTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TreeTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2058f05b7b983387fb16ee50267c21fbde91404")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("TreeTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TreeTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
